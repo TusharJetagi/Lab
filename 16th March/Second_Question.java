b87d4e9f170727cb7b630d32b35414d8d2fd8602
Tushar Jetagi <112852721+TusharJetagi@users.noreply.github.com>
2023-03-16 14:19:20 +0530

    Add files via upload

---
+++ 16th March/Second_Question.java
@@ -0,0 +1,24 @@
+class Second_Question
+{
+public static void main(String args[])
+{
+	System.out.println("The two number is: ");
+
+	int a = 5;
+	int b = 6;
+
+	System.out.println(a + " " + b);
+
+	int sum = a + b;
+	int sub = a-b;
+	int mul = a*b;
+	float div = a/b;
+	int rem = a % b;
+
+	System.out.println("The sum is = " + sum);
+	System.out.println("The sub is = " + sub);
+	System.out.println("The mul is = " + mul);
+	System.out.println("The div is = " + div);
+	System.out.println("The rem is = " + rem);
+
+}}
